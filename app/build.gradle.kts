645728c63486d3d6233dda02cd8d9d190b1e8881
adishtripathi242 <adish.tripathi242@myci.csuci.edu>
2026-03-22 16:23:13 -0700

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
+
+    //calender implementation
+    implementation("com.google.android.gms:play-services-auth:21.5.1")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
