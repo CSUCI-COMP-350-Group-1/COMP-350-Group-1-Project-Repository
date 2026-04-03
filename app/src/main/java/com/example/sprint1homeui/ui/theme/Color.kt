8d661eaf5c0be5a5bd6142a5c27c78275c7a7f57
bull5300 <ChangJonathonP@Outlook.com>
2026-04-03 00:22:09 -0700

    Add: colors for UI

--- app/src/main/java/com/example/sprint1homeui/ui/theme/Color.kt
+++ app/src/main/java/com/example/sprint1homeui/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+//Navigation Colors
+val DarkBackground = Color(0xFF1C1B1F)
+val AppWhite = Color(0xFFFFFFFF)
+val GrayIcon = Color(0xFF888888)
+
+//Accent Colors
+val GreenAccent = Color(0xFF2E7D32)
+val TealAccent = Color(0xFF00ACC1)
+val YellowAccent = Color(0xFFFFB300)
+
+//Background Colors
+val AppBackground = Color(0xFFF0F0F0)
+val ContentBlack = Color(0xFF000000)
